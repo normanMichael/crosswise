a5e1db25d7c2fa4cb78cf0d6fa1d76cdd3972fc5
normanMichael <normanmichaelwayfarer@gmail.com>
2023-07-08 08:39:41 -0500

    generations reference

--- sql/crosswiseDb.sql
+++ sql/crosswiseDb.sql
@@ -20,7 +20,7 @@ CREATE TABLE chapter_reference (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES chapter_reference WRITE;
-INSERT INTO chapter_reference VALUES (1,1,'Matthew 4'),(2,1,'Mark 1'),(3,1,'Luke 4'),(4,1,'John 21'),(5,1,'Ezekiel 47'),(6,1,'Jeremiah 16'),(7,2,'Revelation 13'),(8,3,'Acts 27'),(9,4,'Revelation 21'),(10,4,'Ezekiel 40-48'),(11,4,'Genesis 6-9'),(12,5,'Genesis 14'),(13,6,'Daniel 7'),(14,6,'Daniel 12'),(15,6,'Revelation 11'),(16,6,'Revelation 12'),(17,6,'1 Kings 17-19'),(18,6,'Luke 4'),(19,6,'James 5');
+INSERT INTO `chapter_reference` VALUES (1,1,'Matthew 4'),(2,1,'Mark 1'),(3,1,'Luke 4'),(4,1,'John 21'),(5,1,'Ezekiel 47'),(6,1,'Jeremiah 16'),(7,2,'Revelation 13'),(8,3,'Acts 27'),(9,4,'Revelation 21'),(10,4,'Ezekiel 40-48'),(11,4,'Genesis 6-9'),(12,5,'Genesis 14'),(13,6,'Daniel 7'),(14,6,'Daniel 12'),(15,6,'Revelation 11'),(16,6,'Revelation 12'),(17,6,'1 Kings 17-19'),(18,6,'Luke 4'),(19,6,'James 5'),(20,7,'Matthew 1');
 UNLOCK TABLES;
 
 DROP PROCEDURE IF EXISTS get_book;
